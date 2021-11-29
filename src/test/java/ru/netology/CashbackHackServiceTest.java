2e7a17aafec537948f70267626cc6d422f4908d9
Vasilii Lysenkov <lysenkov2@ya.ru>
2021-11-29 20:58:14 +0300

    Test failing added

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -15,4 +15,14 @@ public class CashbackHackServiceTest {
         assertEquals(actual,expected);
 
     }
+
+    @Test
+    public void shouldCalculateZeroBonus() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual,expected);
+
+    }
 }
